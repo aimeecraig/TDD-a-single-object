1b99dcc438a166d42b6bc01c9ee880fa10f2d358
Aimée Craig <aimeecraig1@gmail.com>
2018-10-02 10:53:10 +0100

    third test written

--- spec/dice-game_spec.rb
+++ spec/dice-game_spec.rb
@@ -8,4 +8,11 @@ describe 'roll' do
   it 'returns a number between 1 and 6' do
     expect(roll).to be_between(1, 6)
   end
+
+  it 'returns a random number' do
+    expect(roll).to be >= 1
+    expect(roll).to be <= 6
+  end
+
+  
 end
